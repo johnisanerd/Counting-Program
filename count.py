1c6dfdf086a5fc420ae5a092c04834b8d5350239
John Cole <29712567+johnisanerd@users.noreply.github.com>
2022-08-17 21:54:59 -0400

    Add in try exception
    When I was trying this, I entered a letter not a number.  It threw an exception.
    
    So I thought I could add a try/except around the functioning code.  I tried a few ways, but I added it inside the while loop and now your code runs perfectly.
    
    If there's an issue or a crash, it tells you there's an exception.

--- count.py
+++ count.py
@@ -1,27 +1,32 @@
+# This program was written to count using the Superduper counter.
 from datetime import datetime
+
 def intro():
     print("Hello.\n Welcome to the... \n (drumroll please!) \n ***drumroll*** \n Superduper counter!")
 def main():
     intro()
     while True:
-        print("Enter the number you want the Superduper counter to count to to below:")
-        num_count = int(input())
-        print("Enter the number you want the Superduper counter to count by below:")
-        num_step = int(input())
-        today = datetime.now()
-        print("Day: ", today.day)
-        print("Month: ", today.month)
-        print("Year: ", today.year)
-        print("Hour: ", today.hour)
-        print("Minute: ", today.minute)
-        print("Second: ", today.second)
-        for i in range(0,num_count+1,num_step):
-            print(i)
-        today = datetime.now()
-        print("Day: ", today.day)
-        print("Month: ", today.month)
-        print("Year: ", today.year)
-        print("Hour: ", today.hour)
-        print("Minute: ", today.minute)
-        print("Second: ", today.second)
+        try:
+            print("Enter the number you want the Superduper counter to count to to below:")
+            num_count = int(input())
+            print("Enter the number you want the Superduper counter to count by below:")
+            num_step = int(input())
+            today = datetime.now()
+            print("Day: ", today.day)
+            print("Month: ", today.month)
+            print("Year: ", today.year)
+            print("Hour: ", today.hour)
+            print("Minute: ", today.minute)
+            print("Second: ", today.second)
+            for i in range(0,num_count+1,num_step):
+                print(i)
+            today = datetime.now()
+            print("Day: ", today.day)
+            print("Month: ", today.month)
+            print("Year: ", today.year)
+            print("Hour: ", today.hour)
+            print("Minute: ", today.minute)
+            print("Second: ", today.second)
+        except:
+            print("Exception!")
 main()
